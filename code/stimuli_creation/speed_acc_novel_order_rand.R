d1bb52d98e483c9eead445feab2fd4df08ff9918
Kyle MacDonald <kyle.macdonald@stanford.edu>
2018-05-05 08:34:38 -0700

    added randomization for distractors and carriers

--- code/stimuli_creation/speed_acc_novel_order_rand.R
+++ code/stimuli_creation/speed_acc_novel_order_rand.R
@@ -1,4 +1,5 @@
 library(here)
+library(stringr)
 library(tidyverse)
 
 ## randomize stuff within a learning block
@@ -24,20 +25,55 @@ create_randomized_exp <- function(n_blocks, block_length, strings) {
 
 
 ## side of screen for the target
-replicate(create_randomized_exp(n_blocks = 4, block_length = 8, strings = c("left", "right")), 
+replicate(create_randomized_exp(n_blocks = n_blocks, 
+                                block_length = n_targets, 
+                                strings = c("left", "right")), 
           n = n_orders) %>% 
   as_data_frame() %>% 
   write_csv(., here::here("experiment/orders/speed_acc_novel_target_side_rand.csv"))
 
 ## side of screen for the target
-replicate(create_randomized_exp(n_blocks = 2, block_length = 4, strings = c("left", "right")), 
+replicate(create_randomized_exp(n_blocks = 2, 
+                                block_length = 4, 
+                                strings = c("left", "right")), 
           n = n_orders) %>% 
   as_data_frame() %>% 
   write_csv(., here::here("experiment/orders/speed_acc_novel_gaze_side_rand.csv"))
 
+## novel names to objects
+# create two lexicons that we will vary across the 
+# four orders
+novel_obj_imgs <- list.files(here::here("experiment/novel_objects/"))
+novel_obj_names <- c("bosa", "pifo", "modi", "toma")
 
-## distractor image name
+lex_1 <- data_frame(name = novel_obj_names, 
+                    image = sample(novel_obj_imgs, size = 4, 
+                                   replace = F))
 
-## carrier phrase
+lex_2 <- data_frame(name = novel_obj_names, 
+                    image = sample(novel_obj_imgs, size = 4, 
+                                   replace = F))
 
-## speaker
+## distractor images 
+# need to sample images randomly without replacement
+# removing images in the lexicon
+distractor_set_lex1 <- setdiff(novel_obj_imgs, lex_1$image)
+distractor_set_lex2 <- setdiff(novel_obj_imgs, lex_2$image)
+n_distractors_block <- 16
+
+data_frame(
+  order_lex1_1 = c(sample(distractor_set_lex1, size = n_distractors_block, replace = F), sample(distractor_set_lex1, size = n_distractors_block, replace = F)),
+  order_lex1_2 = c(sample(distractor_set_lex1, size = n_distractors_block, replace = F), sample(distractor_set_lex1, size = n_distractors_block, replace = F)),
+  order_lex2_1 = c(sample(distractor_set_lex1, size = n_distractors_block, replace = F), sample(distractor_set_lex1, size = n_distractors_block, replace = F)),
+  order_lex2_2 = c(sample(distractor_set_lex1, size = n_distractors_block, replace = F), sample(distractor_set_lex1, size = n_distractors_block, replace = F))
+) %>% 
+  write_csv(here::here("experiment/orders/speed_acc_novel_distractor_imgs_rand.csv"))
+
+## carrier phrases
+carriers <- c("hey", "look")
+replicate(create_randomized_exp(n_blocks = 4, 
+                                block_length = 8, 
+                                strings = carriers), 
+          n = n_orders) %>% 
+  as_data_frame() %>% 
+  write_csv(., here::here("experiment/orders/speed_acc_novel_carriers_rand.csv"))
